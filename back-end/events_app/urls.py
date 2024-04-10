ecfbc69a46eed5cbc8d140c71a9b33917e1bc176
Adrian <adrian.garcia0239@gmail.com>
2024-04-10 13:38:29 -0500

    connect Ngrok, Django, and Slack together. Public url was needed so Slack could ping it.
    connect Ngrok, Django, and Slack together. Public url was needed so Slack could ping it.
    
    connect Ngrok, Django, and Slack together. Public url was needed so Slack could ping it.
    
    deleted .env removed

---
+++ back-end/events_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import Greeting
+
+#* < NGROK_URL >/slack/event/greeting/
+
+urlpatterns = [
+    path("greeting/", Greeting.as_view(), name ="greeting")
+    
+]
