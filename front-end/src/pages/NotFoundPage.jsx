2f571ed50fe8b681b15eab3d5f839a6bf5962813
Adrian <adrian.garcia0239@gmail.com>
2024-04-20 10:33:27 -0500

    Created the foundation by implementing browser router and installing dependencies for the project: React-Router, React Three Fiber, TailwindCSS, React Three Fiber Drei, R3F-Perf

---
+++ front-end/src/pages/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+const NotFoundPage = () => {
+    return (
+        <>
+            <div>
+                <h1>You've accessed an unknown page.</h1>
+                <Link to={"/"}>Home</Link>
+            </div>
+        </>
+    );
+};
+
+export default NotFoundPage;
